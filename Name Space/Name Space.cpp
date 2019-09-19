508e00c1b7f8497da21692a4eb26367ee04c0d99
Kodi Berube <berubek@thomas.edu>
2019-09-19 16:16:25 -0400

    Add project files.

---
+++ Name Space/Name Space.cpp
@@ -0,0 +1,14 @@
+// Name Space.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+int main()
+{
+	cout << "+--------------+" << endl;
+	cout << "| Bob Sacamono |" << endl;
+	cout << "+--------------+" << endl;
+}
